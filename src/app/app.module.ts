e26c22a624545f8e3b2c10986dc283cf1bc2dbdf
Jution Candra Kirana <jutionck@gmail.com>
2020-11-02 14:04:16 +0700

    Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EngineComponent } from './engine/engine.component';
 
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/engine/engine.component.ts
+++ src/app/engine/engine.component.ts
@@ -1,41 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-engine',
   templateUrl: './engine.component.html',
   styleUrls: ['./engine.component.scss']
 })
 export class EngineComponent implements OnInit {
+  engineID =  10;
+  engineStatus = 'off';
 
-  // membuat data binding, yang akan di panggil di .html
-  // engineID =  10;
-  // engineStatus = 'off';
-
-  //  property data binding
-  allowStartedEngine = false;
-  //  Event Binding
-  engineStartedStatus = 'No engine was on!';
-  engineName = '';
   constructor() {
     setTimeout(() => {
       this.allowStartedEngine = true;
     }, 3000);
+    this.engineStatus = Math.random() > 0.5 ? 'on' : 'off';
   }
+
+  // membuat data binding, yang akan di panggil di .html
+  //  property data binding
+  allowStartedEngine = false;
+  //  Event Binding
+  engineStartedStatus = 'No engine was on!';
+  engineName = 'Test Name';
+  engineCreated = false;
+
+  // tslint:disable-next-line:typedef
+  getEngineStatus: any;
 
   ngOnInit(): void {
   }
 
   // tslint:disable-next-line:typedef
   engineStarted() {
-    this.engineStartedStatus = 'Engine was on';
+    this.engineCreated = true;
+    this.engineStatus = 'Engine was on, engine name is ' + this.engineName;
   }
 
   // tslint:disable-next-line:typedef
   updateEngineName(event: Event) {
     console.log(event);
     this.engineName = (event.target as HTMLInputElement).value;
-
   }
 
 }
